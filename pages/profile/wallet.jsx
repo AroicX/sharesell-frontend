12fc8f2318ba36d918da72f9a0cf26532de9ecab
Bamidele Oluwatobi <teebams49@gmail.com>
2021-12-24 13:47:19 -0800

    feat: Add Account Page

--- pages/profile/wallet.jsx
+++ pages/profile/wallet.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import AuthProvider from 'components/AuthProvider';
 import Wallet from 'components/profile/wallet';
 import EmptyAccount from '@/components/profile/empty-account';
+import AddAccount from '@/components/profile/add-account';
 
 export default function WalletPage() {
   const [walletStep, setWalletStep] = useState({ step: 1 });
@@ -21,6 +22,7 @@ export default function WalletPage() {
     <AuthProvider>
       {walletStep.step === 1 ? <Wallet next={next} /> : ''}
       {walletStep.step === 2 ? <EmptyAccount back={back} next={next} /> : ''}
+      {walletStep.step === 3 ? <AddAccount back={back} next={next}/> : ''}
     </AuthProvider>
   );
 }
