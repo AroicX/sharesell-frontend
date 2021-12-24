5da7196a72c56f11b292f65daa27acd7213edefb
Bamidele Oluwatobi <teebams49@gmail.com>
2021-12-24 13:22:27 -0800

    feat: Empty Account State

--- pages/profile/wallet.jsx
+++ pages/profile/wallet.jsx
@@ -1,11 +1,26 @@
-import React from "react"
-import AuthProvider from "components/AuthProvider"
-import Wallet from "components/profile/wallet"
+import React, { useState } from 'react';
+import AuthProvider from 'components/AuthProvider';
+import Wallet from 'components/profile/wallet';
+import EmptyAccount from '@/components/profile/empty-account';
 
-export default function WalletPage () {
-    return (
-        <AuthProvider>
-            <Wallet />
-        </AuthProvider>
-    )
+export default function WalletPage() {
+  const [walletStep, setWalletStep] = useState({ step: 1 });
+  const back = () => {
+    if (walletStep.step > 1) {
+      setWalletStep((prev) => {
+        return { step: prev.step - 1 };
+      });
+    }
+  };
+  const next = () => {
+    setWalletStep((prev) => {
+      return { step: prev.step + 1 };
+    });
+  };
+  return (
+    <AuthProvider>
+      {walletStep.step === 1 ? <Wallet next={next} /> : ''}
+      {walletStep.step === 2 ? <EmptyAccount back={back} next={next} /> : ''}
+    </AuthProvider>
+  );
 }
