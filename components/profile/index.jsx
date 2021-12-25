d55aacc1919b3130b3c32707f2d3467ee268fbe5
Bamidele Oluwatobi <teebams49@gmail.com>
2021-12-25 09:11:23 -0800

    feat: Update Account Page

--- components/profile/index.jsx
+++ components/profile/index.jsx
@@ -14,35 +14,35 @@ export default function Profile() {
       icon: '/svg/history.svg',
       title: 'History',
       detail: 'View transaction and order history',
-      to:'/profile/history'
+      to: '/profile/history',
     },
     {
       icon: '/svg/subscription.svg',
       title: 'Subscription',
       detail: 'Manage your sharesell subscription',
-      to: "profile/subscription"
+      to: 'profile/subscription',
     },
     {
       icon: '/svg/wallet.svg',
       title: 'Make a Withdrawal',
       detail: 'Withdraw cash to your account',
-      to: "/profile/wallet"
+      to: '/profile/wallet',
     },
     {
       icon: '/svg/lock.svg',
       title: 'Change Password',
       detail: 'Manage Password',
-      to: "/profile/change-password"
+      to: '/profile/change-password',
     },
     {
       icon: '/svg/logout.svg',
       title: 'Logout',
       detail: 'Exit account',
-      to: "/"
+      to: '/',
     },
   ];
 
-  const Router = useRouter()
+  const Router = useRouter();
   return (
     <div className='profile'>
       <h2 className='text-3xl font-light my-2'>Profile</h2>
@@ -56,7 +56,10 @@ export default function Profile() {
         <div className='flex flex-col ml-4'>
           <p className='font-semibold text-base'>Chika Inc</p>
           <p className='text-xs text-app-text'>Sirmudiadavid@gmail.com</p>
-          <div className='flex justify-center items-center h-6 min-w-max relative mt-2 rounded-sm border border-pry-black'>
+          <div
+            className='flex justify-center items-center h-6 min-w-max relative mt-2 rounded-sm border border-pry-black cursor-pointer' 
+            onClick={() => Router.push('profile/update-account')}
+          >
             <p className='text-sm px-1'>Update & Activate Account</p>
             <div className='absolute w-2 h-2 rounded-full bg-terms profile-notification'></div>
           </div>
@@ -64,7 +67,11 @@ export default function Profile() {
       </div>
       <div className='profile-options mt-8'>
         {ProfileOptions.map((option, index) => (
-          <div key={index} className='flex pt-5 cursor-pointer' onClick={() => Router.push(option.to)}>
+          <div
+            key={index}
+            className='flex pt-5 cursor-pointer'
+            onClick={() => Router.push(option.to)}
+          >
             <div className='w-30 mr-6'>
               <SVG src={option.icon} className='max-w-full' />
             </div>
---
+++ components/profile/update-account.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import AppHeader from '@/components/AppHeader';
+import UpdateTab from '@/components/update-tab';
+
+export default function UpdateAccount() {
+  const details = [
+    {
+      title: 'Busniess Details',
+      info: 'Business name and other relevant details',
+      progress: '100%',
+      isComplete: true,
+      completeIcon: '/svg/business-complete.svg',
+      inCompleteIcon: '/svg/business-incomplete.svg',
+      to: "/update-account/business"
+    },
+    {
+      title: 'Contact Person',
+      info: 'Provide accurate contact details',
+      progress: '0%',
+      isComplete: false,
+      completeIcon: '/svg/contact-complete.svg',
+      inCompleteIcon: '/svg/contact-incomplete.svg',
+      to: "/update-account/contact"
+    },
+    {
+      title: 'BVN',
+      info: 'Bank Verification Number',
+      progress: '100%',
+      isComplete: true,
+      completeIcon: '/svg/bvn-complete.svg',
+      inCompleteIcon: '/svg/bvn-incomplete.svg',
+      to: "/update-account/bvn"
+    },
+    {
+      title: 'Next Of Kin',
+      info: 'Accurate contact details of Next Of Kin',
+      progress: '0%',
+      isComplete: false,
+      completeIcon: '/svg/kin-complete.svg',
+      inCompleteIcon: '/svg/kin-incomplete.svg',
+      to: "/update-account/kin"
+    },
+  ];
+  return (
+    <div className='mt-4'>
+      <AppHeader noSVG />
+      <div className='mt-3'>
+        <h2 className='text-2xl font-light my-2'>Update & Activate Account</h2>
+        <div className='flex flex-col'>
+          {details.map((detail, index) => (
+            <UpdateTab key={index} detail={detail} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
