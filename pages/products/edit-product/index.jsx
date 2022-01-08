52c4ee36b2489718ada3ee28a6513badade9fba8
Bamidele Oluwatobi <teebams49@gmail.com>
2022-01-08 00:57:07 -0800

    feat: Login Page

--- pages/products/edit-product/index.jsx
+++ pages/products/edit-product/index.jsx
@@ -20,22 +20,30 @@ import {
 } from '@/helpers/index';
 import axios from 'axios';
 import Swal from 'sweetalert2';
-import { CREATE_PRODUCT } from '@/services/products';
+import { GET_SINGLE_PRODUCT } from '@/services/products';
+import Loader from '@/reusable/Loader';
 
 let counter = 0;
 let links = [];
 export default function AddProduct({}) {
   const { productCategories, user, currentProduct } = useGlobalStore();
-  console.log(currentProduct);
 
   useEffect(() => {
-    if (productCategories.length < 1) {
-      router.push('/products');
+    if (productCategories.length < 1 || currentProduct.length < 1) {
+      if (productCategories.length < 1) {
+        router.push('/products');
+      }
+      if (currentProduct.length < 1) {
+        router.back();
+      }
+    } else {
+      getProduct(currentProduct.id);
     }
   }, [productCategories]);
 
   const [images, setImages] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [loader, setLoader] = useState(true);
   const [data, setData] = useState({
     user_id: user ? user.user_id : '',
     product_name: '',
@@ -66,34 +74,36 @@ export default function AddProduct({}) {
     product_images_error: '',
   });
 
-  const handleSubmit = () => {
+  const categoryFormatter = (category_id) => {
+    for (let i = 0; i < productCategories.length; i++) {
+      if (productCategories[i].category_id === category_id) {
+        return productCategories[i].category_name;
+      }
+    }
+  };
+
+  const getProduct = (id) => {
     // event.preventDefault();
-    data.product_images = links;
-    data.product_price = convertPricetoNumber(data.product_price);
-    data.product_retail_price = convertPricetoNumber(data.product_retail_price);
+    // data.product_images = links;
+    // data.product_price = convertPricetoNumber(data.product_price);
+    // data.product_retail_price = convertPricetoNumber(data.product_retail_price);
     const callback = (response) => {
       if (response) {
         setLoading(false);
-        const { product_name } = response.payload;
-        Swal.fire({
-          title: 'Completed',
-          text: response.message,
-          icon: 'success',
-          confirmButtonText: 'View Product',
-          confirmButtonColor: '#A55954',
-          allowOutsideClick: false,
-        }).then((result) => {
-          if (result.dismiss !== 'cancel') {
-            return router.push(`/products/${slugify(product_name)}`);
-          }
-        });
+        let productDetail = response.payload;
+        setData((prev) => ({
+          ...prev,
+          ...productDetail,
+          product_category: categoryFormatter(productDetail.product_category),
+        }));
+        setLoader(false);
       }
     };
     const onError = (error) => {
       console.log(error);
     };
 
-    CREATE_PRODUCT(data, callback, onError);
+    GET_SINGLE_PRODUCT(id, callback, onError);
   };
   const cloudinaryUpload = (file) => {
     const formData = new FormData();
@@ -285,200 +295,217 @@ export default function AddProduct({}) {
   return (
     <AuthProvider className='add-product mt-4'>
       <h2 className='text-3xl font-light my-10'>Edit Product</h2>
-      <form onSubmit={uploadFiles}>
-        <div className='m-1'>
-          <Input
-            label='Product Name'
-            placeholder='Beanie'
-            value={data.product_name}
-            dispatch={(value) => {
-              onChangeHandler(value, 'product_name', 'product_name_error');
-            }}
-            error={data.product_name_error}
-          />
+      {loader ? (
+        <div>
+          <Loader />
         </div>
+      ) : (
+        <form onSubmit={uploadFiles}>
+          <div className='m-1'>
+            <Input
+              label='Product Name'
+              placeholder='Beanie'
+              value={data.product_name}
+              dispatch={(value) => {
+                onChangeHandler(value, 'product_name', 'product_name_error');
+              }}
+              error={data.product_name_error}
+            />
+          </div>
 
-        <div className='m-1'>
-          <Select
-            label='Product Category'
-            placeholder='Select Category'
-            options={selectFilter(
-              productCategories,
-              'category_id',
-              'category_name'
-            )}
-            dispatch={(value) =>
-              onChangeHandler(
-                value,
-                'product_category',
-                'product_category_error'
-              )
-            }
-            error={data.product_category_error}
-          />
-        </div>
+          <div className='m-1'>
+            <Select
+              label='Product Category'
+              placeholder='Select Category'
+              options={selectFilter(
+                productCategories,
+                'category_id',
+                'category_name'
+              )}
+              dispatch={(value) =>
+                onChangeHandler(
+                  value,
+                  'product_category',
+                  'product_category_error'
+                )
+              }
+              initialValue={data.product_category}
+              error={data.product_category_error}
+            />
+          </div>
 
-        <div className='m-1'>
-          <p>Upload product image</p>
-          <span className='text-app-color text-sm'>
-            Please upload 5 different views of the product
-          </span>
-          <DropZone
-            dispatch={(files) => setImagesHandler(files)}
-            error={data.product_images_error}
-          />
-        </div>
+          <div className='m-1'>
+            <p>Upload product image</p>
+            <span className='text-app-color text-sm'>
+              Please upload 5 different views of the product
+            </span>
+            <DropZone
+              dispatch={(files) => setImagesHandler(files)}
+              error={data.product_images_error}
+            />
+          </div>
 
-        <div className='m-1 mt-8'>
-          <TextArea
-            label='Product Description'
-            placeholder='This is like a head warmer'
-            value={data.product_description}
-            dispatch={(value) => {
-              onChangeHandler(
-                value,
-                'product_description',
-                'product_description_error'
-              );
-            }}
-            error={data.product_description_error}
-          />
-        </div>
+          <div className='m-1 mt-8'>
+            <TextArea
+              label='Product Description'
+              placeholder='This is like a head warmer'
+              value={data.product_description}
+              dispatch={(value) => {
+                onChangeHandler(
+                  value,
+                  'product_description',
+                  'product_description_error'
+                );
+              }}
+              error={data.product_description_error}
+            />
+          </div>
 
-        <div className='m-1'>
-          <Input
-            label='Product Price'
-            placeholder='3,000'
-            price={true}
-            value={data.product_price}
-            dispatch={(value) => {
-              onChangeHandler(value, 'product_price', 'product_price_error');
-            }}
-            error={data.product_price_error}
-          />
-        </div>
+          <div className='m-1'>
+            <Input
+              label='Product Price'
+              placeholder='3,000'
+              price={true}
+              value={data.product_price}
+              dispatch={(value) => {
+                onChangeHandler(value, 'product_price', 'product_price_error');
+              }}
+              error={data.product_price_error}
+            />
+          </div>
 
-        <div className='m-1'>
-          <Input
-            label='Product Weight (KG)'
-            placeholder='0.5'
-            value={data.product_weight}
-            dispatch={(value) =>
-              onChangeHandler(value, 'product_weight', 'product_weight_error')
-            }
-            error={data.product_weight_error}
-          />
-        </div>
+          <div className='m-1 mt-6'>
+            <Input
+              label='Product Weight (KG)'
+              placeholder='0.5'
+              value={data.product_weight}
+              dispatch={(value) =>
+                onChangeHandler(value, 'product_weight', 'product_weight_error')
+              }
+              error={data.product_weight_error}
+            />
+          </div>
 
-        <div className='m-1'>
-          <Select
-            label='Product Size'
-            placeholder='Select Size'
-            options={[
-              { id: 'all_sizes', name: 'All Sizes' },
-              { id: 's', name: 'S' },
-              { id: 'm', name: 'M' },
-              { id: 'l', name: 'L' },
-              { id: 'xl', name: 'XL' },
-              { id: 'xxl', name: 'XXL' },
-              { id: 'xxl', name: 'XXXL' },
-            ]}
-            dispatch={(value) => {
-              onChangeHandler(value, 'product_size', 'product_size_error');
-            }}
-            error={data.product_size_error}
-          />
-        </div>
+          <div className='m-1'>
+            <Select
+              label='Product Size'
+              placeholder='Select Size'
+              options={[
+                { id: 'all_sizes', name: 'All Sizes' },
+                { id: 's', name: 'S' },
+                { id: 'm', name: 'M' },
+                { id: 'l', name: 'L' },
+                { id: 'xl', name: 'XL' },
+                { id: 'xxl', name: 'XXL' },
+                { id: 'xxl', name: 'XXXL' },
+              ]}
+              dispatch={(value) => {
+                onChangeHandler(value, 'product_size', 'product_size_error');
+              }}
+              initialValue={data.product_size}
+              error={data.product_size_error}
+            />
+          </div>
 
-        <div className='m-1'>
-          <Select
-            label='Product Quantity'
-            placeholder='Select Quantity'
-            options={[
-              { id: 'limited', name: 'Limited' },
-              { id: 'unlimited', name: 'Unlimited' },
-            ]}
-            dispatch={(value) => {
-              onChangeHandler(
-                value,
-                'product_quantity',
-                'product_quantity_error'
-              );
-            }}
-            error={data.product_quantity_error}
-          />
-        </div>
+          <div className='m-1'>
+            <Select
+              label='Product Quantity'
+              placeholder='Select Quantity'
+              options={[
+                { id: 'limited', name: 'Limited' },
+                { id: 'unlimited', name: 'Unlimited' },
+              ]}
+              dispatch={(value) => {
+                onChangeHandler(
+                  value,
+                  'product_quantity',
+                  'product_quantity_error'
+                );
+              }}
+              initialValue={data.product_quantity}
+              error={data.product_quantity_error}
+            />
+          </div>
 
-        <div className='m-1'>
-          <Input
-            label='Quantity Number'
-            type='number'
-            placeholder='20'
-            value={data.product_number}
-            dispatch={(value) => {
-              onChangeHandler(value, 'product_number', 'product_number_error');
-            }}
-            error={data.product_number_error}
-          />
-        </div>
+          <div className='m-1'>
+            <Input
+              label='Quantity Number'
+              type='number'
+              placeholder='20'
+              value={data.product_number}
+              dispatch={(value) => {
+                onChangeHandler(
+                  value,
+                  'product_number',
+                  'product_number_error'
+                );
+              }}
+              error={data.product_number_error}
+            />
+          </div>
 
-        <div className='m-1 my-5'>
-          <Input
-            label='Suggested Retail Price'
-            placeholder='3,000'
-            price={true}
-            value={data.product_retail_price}
-            dispatch={(value) =>
-              onChangeHandler(
-                value,
-                'product_retail_price',
-                'product_retail_price_error'
-              )
-            }
-            error={data.product_retail_price_error}
-          />
-        </div>
+          <div className='m-1 my-5'>
+            <Input
+              label='Suggested Retail Price'
+              placeholder='3,000'
+              price={true}
+              value={data.product_retail_price}
+              dispatch={(value) =>
+                onChangeHandler(
+                  value,
+                  'product_retail_price',
+                  'product_retail_price_error'
+                )
+              }
+              error={data.product_retail_price_error}
+            />
+          </div>
 
-        <div className='m-1'>
-          <TextArea
-            label='Pickup Address'
-            placeholder='No.3 Maha Close'
-            value={data.pickup_address}
-            dispatch={(value) =>
-              onChangeHandler(value, 'pickup_address', 'pickup_address_error')
-            }
-            error={data.pickup_address_error}
-          />
-        </div>
+          <div className='m-1'>
+            <TextArea
+              label='Pickup Address'
+              placeholder='No.3 Maha Close'
+              value={data.pickup_address}
+              dispatch={(value) =>
+                onChangeHandler(value, 'pickup_address', 'pickup_address_error')
+              }
+              error={data.pickup_address_error}
+            />
+          </div>
 
-        <div className='m-1'>
-          <Select
-            label='State'
-            placeholder='Select State'
-            options={getStates()}
-            placeholder={'Select State'}
-            dispatch={(value) => onChangeHandler(value, 'state', 'state_error')}
-            error={data.state_error}
-          />
-        </div>
-        <div className='m-1'>
-          <Select
-            label='City'
-            placeholder='Select City'
-            options={getCity(data.state)}
-            placeholder={'Select City'}
-            dispatch={(value) => onChangeHandler(value, 'city', 'city_error')}
-            error={data.city_error}
-          />
-        </div>
+          <div className='m-1'>
+            <Select
+              label='State'
+              placeholder='Select State'
+              options={getStates()}
+              placeholder={'Select State'}
+              dispatch={(value) =>
+                onChangeHandler(value, 'state', 'state_error')
+              }
+              error={data.state_error}
+              initialValue={data.state}
+            />
+          </div>
+          <div className='m-1'>
+            <Select
+              label='City'
+              placeholder='Select City'
+              options={getCity(data.state)}
+              placeholder={'Select City'}
+              dispatch={(value) => onChangeHandler(value, 'city', 'city_error')}
+              error={data.city_error}
+              initialValue={data.city}
+            />
+          </div>
 
-        <Button
-          type='submit'
-          text='Save Product'
-          iconRight={'/svg/arrow-right.svg'}
-          loading={loading}
-        />
-      </form>
+          <Button
+            type='submit'
+            text='Save Product'
+            iconRight={'/svg/arrow-right.svg'}
+            loading={loading}
+          />
+        </form>
+      )}
     </AuthProvider>
   );
 }
